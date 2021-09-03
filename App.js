76e16129c6dc79df948a97cbc4472907654127f4
Your Name <you@example.com>
2021-09-03 14:28:56 +0200

    add darkmode

--- App.js
+++ App.js
@@ -1,23 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import deepmerge from 'deepmerge';
 
 const backgroundColor = 'ghostwhite';
 
 export default function App() {
+  const [darkTheme, setDarkTheme] = useState(true);
+  const styles = getStyleSheet(darkTheme);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+          title={"Darkmode: " + darkTheme}
+          onPress={() => setDarkTheme(!darkTheme)}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const baseStyleSheet = {
   container: {
     flex: 1,
     backgroundColor: backgroundColor,
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+};
+
+const lightStyleSheet = {
+  container: {
+    backgroundColor: backgroundColor
+  },
+};
+
+const darkStyleSheet = {
+  container: {
+    backgroundColor: "grey"
+  },
+};
+
+export function getStyleSheet(useDarkTheme){
+  const theme = useDarkTheme ? darkStyleSheet : lightStyleSheet;
+  return StyleSheet.create(deepmerge(baseStyleSheet, theme));
+}
